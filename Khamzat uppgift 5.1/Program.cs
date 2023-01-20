1031108fab5999cfb1514443afde0e0045b5d36d
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:28:38 +0100

    Add project files.

---
+++ Khamzat uppgift 5.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Projekt_5._1_khamzat
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("skriv in 5 olika namn på vad som helst");
+            Console.WriteLine("Börja med nummer fem sedan skriv 5 namn");
+            int allt = int.Parse(Console.ReadLine());
+            string[] namn = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {                           
+                namn[i] = Console.ReadLine();
+            }
+            // skriver ner allt som användare skrev.
+            Console.WriteLine("Här är 5 namn du valde");
+            for (int i = 0; i < allt; i++)
+            {
+                Console.WriteLine(namn[i]);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+
+}
